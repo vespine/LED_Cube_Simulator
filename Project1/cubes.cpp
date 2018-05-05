b020d0b11bdf0995438bd3225473e2610cf53ff5
vespine <github@vespine.com>
2018-05-06 07:12:07 +1000

    small clean up of comments

--- Project1/cubes.cpp
+++ Project1/cubes.cpp
@@ -1,3 +1,4 @@
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -69,7 +70,7 @@ glm::vec3 translation;
 
 
 
-				 // camera setup
+// camera setup
 Camera camera(glm::vec3(0.0f, 0.0f, 7.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
@@ -149,63 +150,56 @@ void func_2()//test thread, not implemented yet
 {
 	while (termin == 0)
 		//test code not implemented
-	{/* 
-		float finv = 0.0f;
-		float stv = 0.0f;
-		float aa = 0;
-		float bb = 0;
-		float cc = 0;
-		float dd = 0;
-		float ee = 0;
-		float ff = 0;
-		float gg = 0;
-		float hh = 0;
-
-		float aa2 = 0;
-		float bb2 = 0;
-		float cc2 = 0;
-		float dd2 = 0;
-		float ee2 = 0;
-		float ff2 = 0;
-		float gg2 = 0;
-		float hh2 = 0;
-		
-		
-		
-		for (int i = 0; i < 8; i++)
-		{
-			
-			aa = display_buffer[8 * i][63];
-			bb = display_buffer[8 * i + 1][63] * 8 / 8;
-			cc = display_buffer[8 * i + 2][63] * 7 / 8;
-			dd = display_buffer[8 * i + 3][63] * 6 / 8;
-			ee = display_buffer[8 * i + 4][63] * 5 / 8;
-			ff = display_buffer[8 * i + 5][63] * 4 / 8;
-			gg = display_buffer[8 * i + 6][63] * 3 / 8;
-			hh = display_buffer[8 * i + 7][63] * 2 / 8;
-
-			bb2 = display_buffer[8 * i - 1][63] * 8 / 8;
-			cc2 = display_buffer[8 * i - 2][63] * 7 / 8;
-			dd2 = display_buffer[8 * i - 3][63] * 6 / 8;
-			ee2 = display_buffer[8 * i - 4][63] * 5 / 8;
-			ff2 = display_buffer[8 * i - 5][63] * 4 / 8;
-			gg2 = display_buffer[8 * i - 6][63] * 3 / 8;
-			hh2 = display_buffer[8 * i - 7][63] * 2 / 8;
-
-
-			finv = aa + bb + cc + dd + ee + ff + gg + hh + aa2 + bb2 + cc2 + dd2 + ee2 + ff2 + gg2 + hh2;
-			
-			
-			display_array[i][56] = finv;
-		
-			std::this_thread::sleep_for(1ms);
-			
-		}
+	{/*
+	 float finv = 0.0f;
+	 float stv = 0.0f;
+	 float aa = 0;
+	 float bb = 0;
+	 float cc = 0;
+	 float dd = 0;
+	 float ee = 0;
+	 float ff = 0;
+	 float gg = 0;
+	 float hh = 0;
+	 float aa2 = 0;
+	 float bb2 = 0;
+	 float cc2 = 0;
+	 float dd2 = 0;
+	 float ee2 = 0;
+	 float ff2 = 0;
+	 float gg2 = 0;
+	 float hh2 = 0;
 
 
 
-		*/ 
-		
+	 for (int i = 0; i < 8; i++)
+	 {
+
+	 aa = display_buffer[8 * i][63];
+	 bb = display_buffer[8 * i + 1][63] * 8 / 8;
+	 cc = display_buffer[8 * i + 2][63] * 7 / 8;
+	 dd = display_buffer[8 * i + 3][63] * 6 / 8;
+	 ee = display_buffer[8 * i + 4][63] * 5 / 8;
+	 ff = display_buffer[8 * i + 5][63] * 4 / 8;
+	 gg = display_buffer[8 * i + 6][63] * 3 / 8;
+	 hh = display_buffer[8 * i + 7][63] * 2 / 8;
+	 bb2 = display_buffer[8 * i - 1][63] * 8 / 8;
+	 cc2 = display_buffer[8 * i - 2][63] * 7 / 8;
+	 dd2 = display_buffer[8 * i - 3][63] * 6 / 8;
+	 ee2 = display_buffer[8 * i - 4][63] * 5 / 8;
+	 ff2 = display_buffer[8 * i - 5][63] * 4 / 8;
+	 gg2 = display_buffer[8 * i - 6][63] * 3 / 8;
+	 hh2 = display_buffer[8 * i - 7][63] * 2 / 8;
+	 finv = aa + bb + cc + dd + ee + ff + gg + hh + aa2 + bb2 + cc2 + dd2 + ee2 + ff2 + gg2 + hh2;
+
+
+	 display_array[i][56] = finv;
+
+	 std::this_thread::sleep_for(1ms);
+
+	 }
+	 */
+
 	}
 }
 
@@ -219,32 +213,25 @@ void func_1() //pattern drawring thread.
 
 
 	//test_pattern
-	/*
 	
+	/*
 	while (1 == 1)
 	{
+	for (int l = 0; l<100; l++)
+	{
+	for (int x = 0; x<8; x++)
+	for (int y = 0; y<8; y++)
+	for (int z = 0; z<8; z++)
+	{
+	set_xhue(x, y, z, h++);
+	if (x == 8) x = 0;
+	if (y == 8) y = 0;
+	if (z == 8) z = 0;
 
-		for (int l = 0; l<100; l++)
-		{
-			for (int x = 0; x<8; x++)
-				for (int y = 0; y<8; y++)
-					for (int z = 0; z<8; z++)
-					{
-						set_xhue(x, y, z, h++);
-						if (x == 8) x = 0;
-						if (y == 8) y = 0;
-						if (z == 8) z = 0;
-						
-
-						std::this_thread::sleep_for(10ms);
-
-
-
-					}
-
-		}//for loop
+	std::this_thread::sleep_for(10ms);
 	}
-
+	}//for loop
+	}
 	*/
 
 
@@ -257,9 +244,10 @@ void func_1() //pattern drawring thread.
 
 
 	threadrunning = TRUE; //init thread flag
+	
 	while (termin == 0) //this doesn't work properly, the solution is std::promise and std::future, need to figure this out 
 	{
-		
+
 		HANDLE hComm;                          // Handle to the Serial port
 		char  ComPortName[] = "\\\\.\\COM1";  // Name of the Serial port(May Change) to be opened,
 		BOOL  Status;                          // Status of the various operations 
@@ -272,7 +260,7 @@ void func_1() //pattern drawring thread.
 		printf("\n\n +==========================================+");
 		printf("\n |    Serial Port  Reception (Win32 API)    |");
 		printf("\n +==========================================+\n");
-		/*---------------------------------- Opening the Serial Port -------------------------------------------*/
+		//---------------------------------- Opening the Serial Port -------------------------------------------
 
 		hComm = CreateFile(ComPortName,                  // Name of the Port to be Opened
 			GENERIC_READ | GENERIC_WRITE, // Read/Write Access
@@ -287,7 +275,7 @@ void func_1() //pattern drawring thread.
 		else
 			printf("\n    Port %s Opened\n ", ComPortName);
 
-		/*------------------------------- Setting the Parameters for the SerialPort ------------------------------*/
+		//------------------------------- Setting the Parameters for the SerialPort ------------------------------
 
 		DCB dcbSerialParams = { 0 };                         // Initializing DCB structure
 		dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
@@ -317,7 +305,7 @@ void func_1() //pattern drawring thread.
 			printf("\n       Parity   = %d", dcbSerialParams.Parity);
 		}
 
-		/*------------------------------------ Setting Timeouts --------------------------------------------------*/
+		//------------------------------------ Setting Timeouts --------------------------------------------------
 
 		COMMTIMEOUTS timeouts = { 0 };
 		timeouts.ReadIntervalTimeout = 50;
@@ -331,7 +319,7 @@ void func_1() //pattern drawring thread.
 		else
 			printf("\n\n    Setting Serial Port Timeouts Successfull");
 
-		/*------------------------------------ Setting Receive Mask ----------------------------------------------*/
+		//------------------------------------ Setting Receive Mask ----------------------------------------------
 
 		Status = SetCommMask(hComm, EV_RXCHAR); //Configure Windows to Monitor the serial device for Character Reception
 
@@ -341,7 +329,7 @@ void func_1() //pattern drawring thread.
 			printf("\n\n    Setting CommMask successfull");
 
 
-		/*------------------------------------ Setting WaitComm() Event   ----------------------------------------*/
+		//------------------------------------ Setting WaitComm() Event   ----------------------------------------
 
 
 
@@ -353,7 +341,7 @@ void func_1() //pattern drawring thread.
 
 			Status = WaitCommEvent(hComm, &dwEventMask, NULL); //Wait for the character to be received
 
-															   /*-------------------------- Program will Wait here till a Character is received ------------------------*/
+			//-------------------------- Program will Wait here till a Character is received ------------------------
 			printf("\n\n    Waiting for Data Reception");
 
 			Status = WaitCommEvent(hComm, &dwEventMask, NULL); //Wait for the character to be received
@@ -365,7 +353,7 @@ void func_1() //pattern drawring thread.
 			else //If  WaitCommEvent()==True Read the RXed data using ReadFile();
 			{
 				//printf("\n\n    Characters Received");
-				
+
 
 
 
@@ -375,25 +363,25 @@ void func_1() //pattern drawring thread.
 				while (1 == 1)
 				{
 
-					
+
 					Status = ReadFile(hComm, &TempChar, sizeof(TempChar), &NoBytesRead, NULL);
 
-				
+
 
 					if (TempChar > 0xef)
 					{
 						char currentrow = TempChar - 0xf0;
 						printf("\n    new row");
 						printf("%d", currentrow);
-						
+
 						for (unsigned char cchan = 0; cchan < 192; cchan++)
 						{
-							
+
 							Status = ReadFile(hComm, &TempChar, sizeof(TempChar), &NoBytesRead, NULL);
 							printf("%x", TempChar);
 							//printf("%d", currentrow);
-							
-							
+
+
 							cvalue = TempChar;
 							cvalue = cvalue / 100;
 
@@ -402,26 +390,26 @@ void func_1() //pattern drawring thread.
 							printf("%c", d);
 
 							display_array[currentrow][cchan] = cvalue;
-							
+
 
 						}
-						
+
 
 					}
-						
+
 
 				}
-				
+
 
 			}
 
 		}
-	
 
 
-	CloseHandle(hComm);//Closing the Serial Port
-	printf("\n +==========================================+\n");
-	//_getch();
+
+		CloseHandle(hComm);//Closing the Serial Port
+		printf("\n +==========================================+\n");
+		//_getch();
 
 
 
@@ -436,6 +424,9 @@ void func_1() //pattern drawring thread.
 
 
 	}//termin
+
+
+	
 }
 
 
@@ -596,44 +587,29 @@ int main()
 		glBindVertexArray(VAO);
 
 		/*sorting not working
-
 		for (std::map<float, glm::vec3>::reverse_iterator it = sorted.rbegin(); it != sorted.rend(); ++it)
 		{
-			glm::mat4 model;
-
-			model = glm::translate(model, it->second);
-
-			ourShader.setMat4("model", model);
-
-
-			rowi = boxindex / 64;
-
-			//using mod operation to get individual R G and B values from the display_array to the correct "box index"
-			redvalue = display_array[rowi][boxindex % 64];
-			greenvalue = display_array[rowi][boxindex % 64 + 64];
-			bluevalue = display_array[rowi][boxindex % 64 + 128];
-
-			//this passes our R G and B values to the vertex shader 
-			ourShader.setFloat("red", redvalue);
-			ourShader.setFloat("green", greenvalue);
-			ourShader.setFloat("blue", bluevalue);
-
-			//I wanted the cubes to be more transparent when off but not so transparent when on. 
-			//just noticed transparency works from one direction but is wrong when looking from the other side. 
-			alphax = cube_transparency + (redvalue + bluevalue + greenvalue) / 3;
-			//if (alphax > 1.0) { alphax = 1.0f; }
-			ourShader.setFloat("alphaf", alphax);
-
-
-
-
-
-
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		
+		glm::mat4 model;
+		model = glm::translate(model, it->second);
+		ourShader.setMat4("model", model);
+		rowi = boxindex / 64;
+		//using mod operation to get individual R G and B values from the display_array to the correct "box index"
+		redvalue = display_array[rowi][boxindex % 64];
+		greenvalue = display_array[rowi][boxindex % 64 + 64];
+		bluevalue = display_array[rowi][boxindex % 64 + 128];
+		//this passes our R G and B values to the vertex shader
+		ourShader.setFloat("red", redvalue);
+		ourShader.setFloat("green", greenvalue);
+		ourShader.setFloat("blue", bluevalue);
+		//I wanted the cubes to be more transparent when off but not so transparent when on.
+		//just noticed transparency works from one direction but is wrong when looking from the other side.
+		alphax = cube_transparency + (redvalue + bluevalue + greenvalue) / 3;
+		//if (alphax > 1.0) { alphax = 1.0f; }
+		ourShader.setFloat("alphaf", alphax);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		*/
-		
+
 		for (boxindex = 0; boxindex < 512; boxindex++)
 		{
 			// calculate the model matrix for each object and pass it to shader before drawing
@@ -661,7 +637,7 @@ int main()
 			alphax = cube_transparency;// +((redvalue + bluevalue + greenvalue) / 6);
 
 
-			//if (alphax > 1.0) { alphax = 1.0f; }
+									   //if (alphax > 1.0) { alphax = 1.0f; }
 			ourShader.setFloat("alphaf", alphax);
 
 
@@ -670,7 +646,7 @@ int main()
 
 
 			glDrawArrays(GL_TRIANGLES, 0, 36);
-		
+
 		}
 
 
